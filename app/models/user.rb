29a71df03e9cd0cf23b3e87a54829c7d8bca1458
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-05-11 22:32:24 -0400

    Add Facebook omniauth gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :confirmable, :lockable, :registerable, :recoverable, :rememberable, :trackable, :validatable#, :omniauthable
+  devise :database_authenticatable, :confirmable, :lockable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
