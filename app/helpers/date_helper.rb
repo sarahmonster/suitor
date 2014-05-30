8a283429dbc2df2e68385cc109b0ac04859f5efe
sarah ✈ semark <sarah@triggersandsparks.com>
2014-05-29 21:41:29 -0400

    Improve date comparisons generally.

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,22 +1,30 @@
 module DateHelper
 
   # take a regular old date and turn it into something human-parseable
-  def friendly_date(date)
-    # today
-    if Date.today > date
-      date.strftime("today at %l:%M %P")  
-    # tomorrow
-    elsif Date.tomorrow > date
-      date.strftime("tomorrow at %l:%M %P")
+  def friendly_date(datetime)
+    date = datetime.to_date
+
+    # yesterday: yesterday
+    if date == Date.yesterday
+      datetime.strftime("yesterday")
+    # any other time in the past
+    elsif date < Date.yesterday
+      return "in the past"
+    # today: today at 5pm
+    elsif date == Date.today
+      datetime.strftime("today at %l:%M %P")
+    # tomorrow: tomorrow at 5pm
+    elsif date == Date.tomorrow 
+      datetime.strftime("tomorrow at %l:%M %P")
     # this week: this Thursday at 5pm
-    elsif Date.current.advance(days: 6) > date
-      date.strftime("this %A at %l:%M %P")      
+    elsif date < Date.current.advance(days: 7)
+      datetime.strftime("this %A at %l:%M %P")      
     # next week: next Thursday, March 28th
-    elsif Date.current.advance(days: 14) > date
-      date.strftime("next %A, %B %d")
+    elsif date < Date.current.advance(days: 14)
+      datetime.strftime("next %A, %B %d")
     # default
     else
-      date.strftime("%B %d")
+      datetime.strftime("%B %d")
     end
   end
 
