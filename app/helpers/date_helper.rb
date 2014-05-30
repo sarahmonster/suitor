b8c764f0e8492c6cfb886e537f944e796ae24ce8
sarah ✈ semark <sarah@triggersandsparks.com>
2014-05-29 23:58:51 -0400

    Don't show the minutes if time is on-the-hour.

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -6,25 +6,33 @@ module DateHelper
 
     # yesterday: yesterday
     if date == Date.yesterday
-      yesterday
+      "yesterday"
     # any other time in the past: 5 days ago
     elsif date < Date.yesterday
       distance_of_time_in_words(date, Date.today) + " ago"
     # today: today at 5pm
     elsif date == Date.today
-      datetime.strftime("today at %l:%M %P")
+      "today at " + short_time(datetime) 
     # tomorrow: tomorrow at 5pm
     elsif date == Date.tomorrow 
-      datetime.strftime("tomorrow at %l:%M %P")
+      "tomorrow at " + short_time(datetime)
     # this week: this Thursday at 5pm
     elsif date < Date.current.advance(days: 7)
-      datetime.strftime("this %A at %l:%M %P")      
+      datetime.strftime("this %A at ") + short_time(datetime)    
     # next week: next Thursday, March 28th
     elsif date < Date.current.advance(days: 14)
       datetime.strftime("next %A, %B %d")
     # default
     else
       datetime.strftime("%B %d")
+    end
+  end
+
+  def short_time(time)
+    if time.min == 0
+      time.strftime("%l %P")
+    else
+      time.strftime("%l:%M %P")
     end
   end
 
