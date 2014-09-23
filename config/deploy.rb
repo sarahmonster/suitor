b33d4d323a83d938e1363c2b9e4a329d3fbb6a49
sarah semark <sarah@triggersandsparks.com>
2014-09-23 04:05:29 -0400

    Allow for declaring a branch via Capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@github.com:sarahsemark/suitor.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :branch, ENV['BRANCH'] || "master"
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deploy/suitor'
