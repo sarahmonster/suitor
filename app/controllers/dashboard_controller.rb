5ed1980920652d327a2c77dd4ea3ea4f47ad3ed1
sarah semark <sarah@triggersandsparks.com>
2014-09-25 01:01:39 -0400

    List jobs that need an application, and number of follow-ups required.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   def index
     @postings = Posting.all
     @postings_with_upcoming_interviews = Posting.interview_scheduled
+    @postings_needing_followup = Posting.without_followup
+    @postings_needing_application = Posting.havent_applied
   end
 end
