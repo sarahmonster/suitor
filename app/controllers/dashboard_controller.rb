503df936c5cf5131cdca85079eb33a8dd260987e
sarah semark <sarah@triggersandsparks.com>
2014-09-25 01:24:41 -0400

    Add some preliminary prettiness to dashboard action-panels.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,6 @@ class DashboardController < ApplicationController
     @postings_with_upcoming_interviews = Posting.interview_scheduled
     @postings_needing_followup = Posting.without_followup
     @postings_needing_application = Posting.havent_applied
+    @postings_with_upcoming_deadlines = Posting.havent_applied.deadline_approaching
   end
 end
