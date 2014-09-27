4b64b3ffc8eb75313e4ebc167ff7907aabe2b514
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-09-26 23:03:53 -0400

    Show current user in footnotes

--- config/initializers/rails_footnotes.rb
+++ config/initializers/rails_footnotes.rb
@@ -9,6 +9,7 @@ defined?(Footnotes) && Footnotes.setup do |f|
 
   # Only toggle some notes :
   # f.notes = [:session, :cookies, :params, :filters, :routes, :env, :queries, :log, :general]
+  f.notes += [:current_user]
 
   # Change the prefix :
   # f.prefix = 'mvim://open?url=file://%s&line=%d&column=%d'
@@ -24,4 +25,37 @@ defined?(Footnotes) && Footnotes.setup do |f|
 
   # Allow to open multiple notes :
   # f.multiple_notes = true
+end
+
+if defined?(Footnotes)
+  module Footnotes
+    module Notes
+      class CurrentUserNote < AbstractNote
+        # This method always receives a controller
+        #
+        def initialize(controller)
+          @current_user = controller.instance_variable_get("@current_user")
+        end
+
+        # Returns the title that represents this note.
+        #
+        def title
+          "Current user: #{@current_user.email}"
+        end
+
+        # This Note is only valid if we actually found an user
+        # If it's not valid, it won't be displayed
+        #
+        def valid?
+          @current_user
+        end
+
+        # The fieldset content
+        #
+        def content
+          escape(@current_user.inspect)
+        end
+      end
+    end
+  end
 end
