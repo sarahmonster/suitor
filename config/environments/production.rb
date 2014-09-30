7c9297d98e3a38c8d6a3231951de45d4c267c7f7
sarah semark <sarah@triggersandsparks.com>
2014-09-29 20:32:58 -0400

    Set up Action Mailer to use Mandrill on both production and development.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,20 @@ Suitor::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Use Mandrill for email
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'mrsuitor.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"],
+    :enable_starttls_auto => true, 
+    :authentication => 'login', 
+    :domain => 'mrsuitor.com'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
