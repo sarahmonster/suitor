5729ad3af0c6b8d2f50a8349c13bd59c16919ae8
sarah semark <sarah@triggersandsparks.com>
2014-10-04 00:21:48 -0400

    Use names for invites, write some friendly text, and show email address.

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,3 +1,13 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_filter :configure_permitted_parameters
   before_filter :require_admin, only: [:new, :remove, :destroy]
+
+  protected
+
+    def configure_permitted_parameters
+      # Add user settings as allowed params for invites.
+      [:accept_invitation, :invite].each do |c|
+        devise_parameter_sanitizer.for(c).concat [:name]
+      end
+    end
 end
