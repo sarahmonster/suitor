f1278778175abdd474a3197d3872c98038082b25
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-10-04 14:16:45 -0400

    Disable sign-up route in production

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :redirect_to_root
-
   protected
 
     def after_inactive_sign_up_path_for(resource)
-      home_path#(resource)
+      home_path
     end
 
     def after_sign_up_path_for(resource)
-      home_path#(resource)
-    end
-
-    def redirect_to_root
-      redirect_to root_path
+      home_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Suitor::Application.routes.draw do
-
   devise_for :users, skip: [:sessions], controllers: {
     confirmations: 'users/confirmations',
     invitations: 'users/invitations',
@@ -7,10 +6,12 @@ Suitor::Application.routes.draw do
     registrations: 'users/registrations'
   }
   as :user do
-    get 'login' => 'devise/sessions#new', :as => :new_user_session
-    post 'login' => 'devise/sessions#create', :as => :user_session
-    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
-    get 'sign-up' => 'users/registrations#new', as: :user_signup
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    unless Rails.env.production?
+      get 'sign-up' => 'users/registrations#new', as: :user_signup
+    end
     get 'use-invite' => 'users/invitations#edit', as: :invitation_accept
     get 'invite' => 'users/invitations#new', as: :new_invitation
   end
