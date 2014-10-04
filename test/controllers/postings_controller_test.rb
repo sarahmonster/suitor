77e231d38b88510fc476d3a552573d73da9b19a5
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-10-04 01:13:34 -0400

    Add some working tests, cool! (#33, #34)

--- test/controllers/postings_controller_test.rb
+++ test/controllers/postings_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PostingsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "should redirect to login if not authorized to see postings" do
+    get :index
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get index if authorized" do
+    sign_in @user
+
+    get :index
+    assert_response :success
+
+    # Sent to the view to make cacheable views
+    assert_not_nil assigns(:method)
+    assert_not_nil assigns(:sortorder)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,16 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
 
+class ActionController::TestCase
+  include Devise::TestHelpers
+
+  def sign_in(*args)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+
+    super(*args)
+  end
+end
+
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
