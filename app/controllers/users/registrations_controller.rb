7f3813879954414f70524d525b70d8e8bd45a484
sarah semark <sarah@triggersandsparks.com>
2014-10-05 01:55:39 -0400

    Allow users to adjust name, followup offset, and application goal.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters
   before_filter :redirect_if_in_production
 
   protected
@@ -13,5 +14,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     def redirect_if_in_production
       redirect_to root_path if Rails.env.production?
+    end
+
+    def configure_permitted_parameters
+      # Add user settings as allowed params for editing
+      [:sign_up, :account_update].each do |c|
+        devise_parameter_sanitizer.for(c).concat [:name, :followup_offset, :application_goal]
+      end
     end
 end
