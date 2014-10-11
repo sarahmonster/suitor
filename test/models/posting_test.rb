61999c4a9b9c2fff22ce1d332720f946949f5242
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-10-11 01:16:18 -0400

    Add Posting unit test

--- test/models/posting_test.rb
+++ test/models/posting_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "posting needs a title, company, and user" do
+    posting = Posting.new
+    assert_not posting.save, "Saved posting without a title and company"
+
+    posting = Posting.new(title: "Y2K Analyst")
+    assert_not posting.save, "Saved posting without a company"
+
+    posting = Posting.new(hiring_organization: "Initech")
+    assert_not posting.save, "Saved posting without a title"
+
+    posting = Posting.new(title: "Y2K Analyst", hiring_organization: "Initech")
+    assert_not posting.save, "Saved posting without a user"
+
+    posting = Posting.new(title: "Y2K Analyst", hiring_organization: "Initech",
+                          user: users(:one))
+    assert posting.save, "Posting should save with all required properties"
+  end
 end
