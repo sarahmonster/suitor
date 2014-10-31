4e37317d311145abbd1250d45387edd5c87108b4
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-10-31 14:44:27 -0400

    Add Facebook tests with capybara

--- test/integration/dashboard_test.rb
+++ test/integration/dashboard_test.rb
@@ -2,38 +2,38 @@ require 'test_helper'
 
 class DashboardTest < ActionDispatch::IntegrationTest
   test "login and use dashboard" do
-    get "/login"
+    get login_path
     assert_response :success
 
-    post_via_redirect "/login", user: {
+    post_via_redirect login_path, user: {
       email: users(:one).email,
       password: "thisismypassword"
     }
-    assert_equal "/", path
+    assert_equal root_path, path
   end
 
   test "login and use dashboard, create a posting and apply to it, then visit
         the dashboard again" do
-    get "/login"
+    get login_path
     assert_response :success
 
-    post_via_redirect "/login", user: {
+    post_via_redirect login_path, user: {
       email: users(:no_postings).email,
       password: "thisismypassword"
     }
-    assert_equal "/", path
+    assert_equal root_path, path
 
-    get "/postings/new"
+    get new_posting_path
     assert_response :success
 
-    post_via_redirect "/postings", posting: {
+    post_via_redirect postings_path, posting: {
       title: "Y2K Analyst", hiring_organization: "Initech"
     }
     assert_match /\/postings\/\d+/, path
     path_to_posting = path
     path_to_create_job_application = "#{path}/job_applications"
 
-    get "/postings"
+    get postings_path
     assert_response :success
 
     post_via_redirect path_to_create_job_application, job_application: {
@@ -41,7 +41,7 @@ class DashboardTest < ActionDispatch::IntegrationTest
     }
     assert_equal path_to_posting, path
 
-    get "/"
+    get root_path
     assert_response :success
   end
 end
